b11f85b614437f2d9aa0dfe50f68e6b711a05984
HH8023 <wp_0357@163.com>
2018-06-24 19:08:41 +0800

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Wandx') - Blog</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 Route::post('/deploy','DeploymentController@deploy');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
